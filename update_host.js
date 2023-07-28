1390b959b150a8c33cd6355c995adce4343a4b38
ccckj <33249911+ccckj@users.noreply.github.com>
2023-07-28 21:48:26 +0800

    Update update_host.js

--- update_host.js
+++ update_host.js
@@ -31,7 +31,7 @@
 
 var modifiedHeaders = $request.headers;
 modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.3 Mobile/15E148 Safari/604.1';
-modifiedHeaders['Host'] = "listen.10155.com";
+// modifiedHeaders['Host'] = "listen.10155.com";
 
 $done({headers : modifiedHeaders});
 // const $tool = new Tool()
