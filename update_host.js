a7ad0e7c2808fb8893df230c89c869b54b1b98ff
ccckj <33249911+ccckj@users.noreply.github.com>
2023-07-28 21:58:04 +0800

    Update update_host.js

--- update_host.js
+++ update_host.js
@@ -29,76 +29,74 @@
 // });
 
 
-var modifiedHeaders = $request.headers;
-modifiedHeaders['User-Agent'] = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.3 Mobile/15E148 Safari/604.1";
-modifiedHeaders['Host'] = "listen.10155.com";
 
-$done({headers : modifiedHeaders});
 // const $tool = new Tool()
 // const consoleLog = false;
 
-// if (!$tool.isResponse) {
-//     $request.headers["User-Agent"] = ""
+if (!$tool.isResponse) {
+    var modifiedHeaders = $request.headers;
+    modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.3 Mobile/15E148 Safari/604.1';
+    modifiedHeaders['Referer'] = "listen.10155.com";
 
-//     $done({});
+    $done({headers : modifiedHeaders});
 
-// }
+}
 
 
 
-// function Tool() {
-//     _node = (() => {
-//         if (typeof require == "function") {
-//             const request = require('request')
-//             return ({ request })
-//         } else {
-//             return (null)
-//         }
-//     })()
-//     _isSurge = typeof $httpClient != "undefined"
-//     _isQuanX = typeof $task != "undefined"
-//     this.isSurge = _isSurge
-//     this.isQuanX = _isQuanX
-//     this.isResponse = typeof $response != "undefined"
-//     this.notify = (title, subtitle, message) => {
-//         if (_isQuanX) $notify(title, subtitle, message)
-//         if (_isSurge) $notification.post(title, subtitle, message)
-//         if (_node) console.log(JSON.stringify({ title, subtitle, message }));
-//     }
-//     this.write = (value, key) => {
-//         if (_isQuanX) return $prefs.setValueForKey(value, key)
-//         if (_isSurge) return $persistentStore.write(value, key)
-//     }
-//     this.read = (key) => {
-//         if (_isQuanX) return $prefs.valueForKey(key)
-//         if (_isSurge) return $persistentStore.read(key)
-//     }
-//     this.get = (options, callback) => {
-//         if (_isQuanX) {
-//             if (typeof options == "string") options = { url: options }
-//             options["method"] = "GET"
-//             $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
-//         }
-//         if (_isSurge) $httpClient.get(options, (error, response, body) => { callback(error, _status(response), body) })
-//         if (_node) _node.request(options, (error, response, body) => { callback(error, _status(response), body) })
-//     }
-//     this.post = (options, callback) => {
-//         if (_isQuanX) {
-//             if (typeof options == "string") options = { url: options }
-//             options["method"] = "POST"
-//             $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
-//         }
-//         if (_isSurge) $httpClient.post(options, (error, response, body) => { callback(error, _status(response), body) })
-//         if (_node) _node.request.post(options, (error, response, body) => { callback(error, _status(response), body) })
-//     }
-//     _status = (response) => {
-//         if (response) {
-//             if (response.status) {
-//                 response["statusCode"] = response.status
-//             } else if (response.statusCode) {
-//                 response["status"] = response.statusCode
-//             }
-//         }
-//         return response
-//     }
-// }
+function Tool() {
+    _node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
+    _isSurge = typeof $httpClient != "undefined"
+    _isQuanX = typeof $task != "undefined"
+    this.isSurge = _isSurge
+    this.isQuanX = _isQuanX
+    this.isResponse = typeof $response != "undefined"
+    this.notify = (title, subtitle, message) => {
+        if (_isQuanX) $notify(title, subtitle, message)
+        if (_isSurge) $notification.post(title, subtitle, message)
+        if (_node) console.log(JSON.stringify({ title, subtitle, message }));
+    }
+    this.write = (value, key) => {
+        if (_isQuanX) return $prefs.setValueForKey(value, key)
+        if (_isSurge) return $persistentStore.write(value, key)
+    }
+    this.read = (key) => {
+        if (_isQuanX) return $prefs.valueForKey(key)
+        if (_isSurge) return $persistentStore.read(key)
+    }
+    this.get = (options, callback) => {
+        if (_isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
+        }
+        if (_isSurge) $httpClient.get(options, (error, response, body) => { callback(error, _status(response), body) })
+        if (_node) _node.request(options, (error, response, body) => { callback(error, _status(response), body) })
+    }
+    this.post = (options, callback) => {
+        if (_isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => { callback(null, _status(response), response.body) }, reason => callback(reason.error, null, null))
+        }
+        if (_isSurge) $httpClient.post(options, (error, response, body) => { callback(error, _status(response), body) })
+        if (_node) _node.request.post(options, (error, response, body) => { callback(error, _status(response), body) })
+    }
+    _status = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+}
